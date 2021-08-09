21129672d3d254a61ea0bd69097f847fcbf4d77e
samzktube <88573466+samzktube@users.noreply.github.com>
2021-08-09 22:16:35 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -127,7 +127,7 @@ module.exports = {
                 '/dev/nfts',
                 '/dev/swaps',
                 '/dev/events',
-                '/dev/test
+                '/dev/test'
               ],
         }
 
