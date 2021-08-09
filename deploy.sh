9a02fbd6e9cf30ac3612fd29d06260af7e527aea
samzktube <88573466+samzktube@users.noreply.github.com>
2021-08-09 20:02:56 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,19 +7,20 @@ npm install
 npm run docs:build
 
 # 进入生成的文件夹
-# cd docs/.vuepress/dist
+ cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
-
-# git init
-# git add -A
- # git commit -m 'deploy'
+git config --global user.name 'samzktube'
+git config --global user.email 'sam@zktube.io'
+ git init
+ git add -A
+ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io  USERNAME=你的用户名 
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+ git push -f git@github.com:samzktube/docs-test.git master:gh-pages
 
 cd -
