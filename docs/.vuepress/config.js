235cbbe0023645f2fe6bb588963759ac2bc800b4
damon0101 <88707956+damon0101@users.noreply.github.com>
2021-08-10 16:28:54 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -128,7 +128,7 @@ module.exports = {
                 '/dev/swaps',
                 '/dev/events',
                 '/dev/test',
-                '/dev/testdemo'
+                '/dev/testdemo'，
                 '/dev/damontest'
               ],
         }
